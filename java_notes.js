db82030506c7d6b3a0c82c8027650fb5ae3d3c69
Robert Stewart <robert.stewart@leonardo.com>
2017-07-20 16:25:58 -0400

    .

--- java_notes.js
+++ java_notes.js
@@ -496,3 +496,22 @@ function multiplyAll(arr) {
 
 // Modify values below to test your code
 multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+
+
+
+
+
+//Iterate with JavaScript While Loops
+//You can run the same code multiple times by using a loop.
+//Another type of JavaScript loop is called a "while loop", because it runs "while" a specified condition is true and stops once that condition is no longer true.
+
+var myArray = [];
+
+var i = 0;
+while (i <  5) {
+  myArray.push(i);
+  i++;
+}
+// myArray will now contain [0,1,2,3,4]
+
